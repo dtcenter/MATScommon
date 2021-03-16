e4fd916add544d14b01bc74e27a84c83d6fc282a
Molly Smith <molly.b.smith@noaa.gov>
2021-03-16 12:35:53 -0600

    Added raobamdar app

--- meteor_packages/mats-common/templates/common/select.js
+++ meteor_packages/mats-common/templates/common/select.js
@@ -22,12 +22,10 @@ Template.select.onRendered( function () {
         elem && elem.addEventListener('axisRefresh', function (event) {
             // Don't know why I have to do this, I expected the parameter data to be in the context....
             if (matsCollections[this.name] !== undefined) {
-                const param = matsParamUtils.getParameterForName(this.name);
                 const paramData = matsCollections[this.name].findOne({name: this.name}, {
                     dependentNames: 1,
                     peerName: 1
                 });
-                matsSelectUtils.checkHideOther(param, false);
                 matsSelectUtils.refreshPeer(event, paramData);
             }
         });
@@ -39,8 +37,6 @@ Template.select.onRendered( function () {
         }
         elem.options = [];
         elem && elem.addEventListener('refresh', function (event) {
-            const param = matsParamUtils.getParameterForName(this.name);
-            matsSelectUtils.checkHideOther(param,false); // calls checkDisable
             matsSelectUtils.refresh(event,this.name);
         });
     } catch (e) {
