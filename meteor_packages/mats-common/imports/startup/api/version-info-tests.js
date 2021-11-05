30c29fa385e5157c8836d9c28a60f8f06b1015e8
Ian McGinnis <ian.mcginnis@noaa.gov>
2021-11-05 09:39:32 -0600

    Run auto-formatter over the test file

--- meteor_packages/mats-common/imports/startup/api/version-info-tests.js
+++ meteor_packages/mats-common/imports/startup/api/version-info-tests.js
@@ -1,4 +1,4 @@
-import {versionInfo} from 'meteor/randyp:mats-common';
+import { versionInfo } from 'meteor/randyp:mats-common';
 var assert = require('assert');
 
 describe('getVersionsFromEnv', function () {
@@ -10,9 +10,9 @@ describe('getVersionsFromEnv', function () {
     });
 
     // Reset the env after each test
-    afterEach(function() {
+    afterEach(function () {
         Object.keys(process.env).forEach((key) => { delete process.env[key] });
-        Object.entries(envCache).forEach(([ key, value ]) => {
+        Object.entries(envCache).forEach(([key, value]) => {
             if (key !== undefined) {
                 process.env[key] = value;
             }
@@ -20,22 +20,22 @@ describe('getVersionsFromEnv', function () {
     });
 
     // Test
-    it('Correctly reads version from env', function (){
+    it('Correctly reads version from env', function () {
         process.env.VERSION = "4.2.0";
         const { version, commit, branch } = versionInfo.getVersionsFromEnv();
         assert.equal(version, "4.2.0");
     });
-    it('Correctly reads commit from env', function (){
+    it('Correctly reads commit from env', function () {
         process.env.COMMIT = "ae214rfda";
         const { version, commit, branch } = versionInfo.getVersionsFromEnv();
         assert.equal(commit, "ae214rfda");
     });
-    it('Correctly reads version from env', function (){
+    it('Correctly reads version from env', function () {
         process.env.BRANCH = "test";
         const { version, commit, branch } = versionInfo.getVersionsFromEnv();
         assert.equal(branch, "test");
     });
-    it('Correctly handles no env', function (){
+    it('Correctly handles no env', function () {
         const { version, commit, branch } = versionInfo.getVersionsFromEnv();
         assert.equal(version, "Unknown");
         assert.equal(commit, "Unknown");
