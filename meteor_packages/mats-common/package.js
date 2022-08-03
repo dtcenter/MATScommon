1e3bbfe64c5bffa7d3c93dd829de9ca1b738eaf0
Ian McGinnis <ian.mcginnis@noaa.gov>
2022-08-03 15:29:45 -0600

    Bump the default Meteor version to 2.7
    This way we use the latest version of the core packages unless we
    override the version in the import statement in `api.use()`.

--- meteor_packages/mats-common/package.js
+++ meteor_packages/mats-common/package.js
@@ -15,7 +15,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('2.3');
+    api.versionsFrom('2.7');
     Npm.depends({
         'fs-extra': '7.0.0',
         "@babel/runtime": "7.10.4",
