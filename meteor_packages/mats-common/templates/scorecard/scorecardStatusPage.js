eae2a332ab45146f23db0201c81279641f92780c
randy.pierce <randy.pierce@noaa.gov>
2022-10-04 15:59:33 -0600

    getting end to end steel thread working for displayStatusPage

---
+++ meteor_packages/mats-common/templates/scorecard/scorecardStatusPage.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Colorado State University and Regents of the University of Colorado. All rights reserved.
+ */
+
+Template.scorecardStatusPage.helpers({
+    image: function () {
+        var img = "underConstruction.jpg";
+        return img;
+    }
+});
