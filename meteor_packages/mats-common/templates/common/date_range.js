a4559a074f1fbef51d47b9f6977edbe4b6694764
Molly Smith <molly.b.smith@noaa.gov>
2022-10-25 13:02:58 -0600

    Automated tests work with new date widget

--- meteor_packages/mats-common/templates/common/date_range.js
+++ meteor_packages/mats-common/templates/common/date_range.js
@@ -88,6 +88,7 @@ Template.dateRange.onRendered(function () {
     $( ".drp-buttons" ).each(function (index) {
         if ($(this).find("span.newRangeLabel").length === 0) {
             $(this).prepend("<span class='newRangeLabel' style='text-align: right; font-size: 16px;'>Apply this range?&nbsp;&nbsp;</span>");
+            $(this).find('button.applyBtn').attr("id", name + "-applyBtn");
         }
     });
 
