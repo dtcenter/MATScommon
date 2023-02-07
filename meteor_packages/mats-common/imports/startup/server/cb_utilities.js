0727524905f8ff8b53f24f5c96df44703017d712
randytpierce <randy.pierce@noaa.gov>
2023-02-07 15:26:02 -0700

    Update cb_utilities.js
    post merge randy and gopa with scorecard display

--- meteor_packages/mats-common/imports/startup/server/cb_utilities.js
+++ meteor_packages/mats-common/imports/startup/server/cb_utilities.js
@@ -51,23 +51,17 @@ class CBUtilities
         }
     }
 
-    upsertCB = async (key, doc) =>
-    {
-        const couchbase = require("couchbase");
-        try
-        {
-            const conn = await this.getConnection();
-            const result = await conn.collection.upsert(key, doc, {
-                expiry: 60,
-                persist_to: 1
-            });
-            return result;
-        } catch (err)
-        {
-            console.log("upsertCB ERROR: ", err);
-            throw new Error("upsertCB ERROR: " + err);
-        }
-    };
+  upsertCB = async (key, doc) => {
+    const couchbase = require("couchbase");
+    try {
+      const conn = await this.getConnection();
+      const result = await conn.collection.upsert(key, doc);
+      return result;
+    } catch (err) {
+      console.log("upsertCB ERROR: ", err);
+      throw new Error("upsertCB ERROR: " + err);
+    }
+  };
 
     removeCB = async (key) =>
     {
@@ -141,77 +135,4 @@ class CBUtilities
     };
 }
 
-const test = async () =>
-{
-    const host = "adb-cb2.gsd.esrl.noaa.gov,adb-cb3.gsd.esrl.noaa.gov,adb-cb4.gsd.esrl.noaa.gov";
-    //const host = "adb-cb1.gsd.esrl.noaa.gov";
-    const bucketName = "travel-sample";
-    const user = "auser";
-    const pwd = "apassword";
-    cbUtilities = new CBUtilities(host, bucketName, user, pwd);
-
-    const airline = {
-        type: "airline",
-        id: 8091,
-        callsign: "CBS",
-        iata: null,
-        icao: null,
-        name: "Couchbase Airways",
-    };
-    const key = `${airline.type}_${airline.id}`;
-    const statement = "select * from `travel-sample` where meta().id = '" + key + "';"
-
-    try
-    {
-        const time = await cbUtilities.queryCB("select NOW_MILLIS() as time;")
-        console.log("queryCB: ", time);
-    } catch (err)
-    {
-        console.log(err);
-    }
-
-    try
-    {
-        const ret = await cbUtilities.upsertCB(key, airline)
-        console.log("upsertCB: ", ret);
-    } catch (err)
-    {
-        console.log(err);
-    }
-
-    try
-    {
-        const ret1 = await cbUtilities.getCB(key)
-        console.log("getCB: ", ret1);
-    } catch (err)
-    {
-        console.log(err);
-    }
-
-    try
-    {
-        const ret2 = await cbUtilities.queryCB(statement)
-        console.log("queryCB: ", ret2);
-    } catch (err)
-    {
-        console.log(err);
-    }
-
-    try
-    {
-        const ret3 = await cbUtilities.removeCB(key)
-        console.log("deleteCB: ", ret3);
-    } catch (err)
-    {
-        console.log(err);
-    }
-
-    await cbUtilities.closeConnection();
-    process.exit();
-}
-
-export default matsCouchbaseUtils = {
-    CBUtilities: CBUtilities,
-    test: test
-}
 
