2ced2af5c6caccfa1b9a16640f7cabcf8d97ec9f
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2023-04-05 20:39:23 +0000

    update cb SDK to 4.1.3

--- meteor_packages/mats-common/package.js
+++ meteor_packages/mats-common/package.js
@@ -29,7 +29,7 @@ Package.onUse(function (api) {
         "csv-stringify": "4.3.1",
         "node-file-cache" : "1.0.2",
         "python-shell": "5.0.0",
-        "couchbase": "3.2.3",
+        "couchbase": "4.1.3",
         "simpl-schema": "3.4.1",
         "vanillajs-datepicker":"1.3.1",
         "daterangepicker": "3.1.0",
